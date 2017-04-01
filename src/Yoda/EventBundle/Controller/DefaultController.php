35312ca3eb683197ee050b966c1c33aae6cb5911
sungvadan <svd.phan@gmail.com>
2017-04-01 19:18:13 +0200

    controller

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($count,$firstname)
     {
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render(
+          'EventBundle:Default:index.html.twig',
+            array(
+                'name' => $firstname
+            )
+        );
     }
 }
