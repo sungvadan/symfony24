7d69a51eb8b0409c00a073887247b96eb7d36d15
sungvadan <svd.phan@gmail.com>
2017-04-02 10:15:12 +0200

    add some fixture

---
+++ src/Yoda/EventBundle/DataFixtures/ORM/LoadEvents.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EventBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Yoda\EventBundle\Entity\Event;
+
+class LoadEventsData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $event = new Event();
+        $event->setName('Darth\'s surprise birthday party');
+        $event->setLocation('Deathstar');
+        $event->setTime(new \DateTime('tomorrow noon'));
+        $manager->persist($event);
+        
+        $manager->flush();
+    }
+}
