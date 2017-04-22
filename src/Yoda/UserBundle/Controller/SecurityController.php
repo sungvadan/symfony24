e94234984f6032aaa74b68745f6c08675f7abc38
sungvadan <svd.phan@gmail.com>
2017-04-22 19:51:16 +0200

    logout form

--- src/Yoda/UserBundle/Controller/SecurityController.php
+++ src/Yoda/UserBundle/Controller/SecurityController.php
@@ -17,15 +17,9 @@ class SecurityController extends Controller
     public function loginAction(Request $request)
     {
         $session = $request->getSession();
-        // get the login error if there is one
-        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(
-                SecurityContextInterface::AUTHENTICATION_ERROR
-            );
-        } else {
-            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
-        }
+
+        $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+        $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
 
         return array(
             'last_username' => $session->get(SecurityContextInterface::LAST_USERNAME),
@@ -38,6 +32,14 @@ class SecurityController extends Controller
      * @Route(path="/login_check", name="login_check")
      */
     public function loginCheckAction()
+    {
+
+    }
+
+    /**
+     * @Route(path="/logout", name="logout")
+     */
+    public function logoutAction()
     {
 
     }
