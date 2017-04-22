66da7166c78fcf969d3246febae3d4b43d33c9eb
sungvadan <svd.phan@gmail.com>
2017-04-22 18:45:50 +0200

    login form

---
+++ src/Yoda/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Yoda\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route(path="/login", name="login_form")
+     * @Template()
+     */
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $session->get(SecurityContextInterface::LAST_USERNAME),
+            'error' => $error,
+        );
+
+    }
+
+    /**
+     * @Route(path="/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+
+    }
+
+}
