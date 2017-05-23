642a06664b5a7672f8a03a7036f5dfe97da424a5
sungvadan <svd.phan@gmail.com>
2017-05-23 11:36:42 +0200

    service container

--- src/Yoda/EventBundle/Controller/ReportController.php
+++ src/Yoda/EventBundle/Controller/ReportController.php
@@ -4,22 +4,20 @@ namespace Yoda\EventBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Yoda\EventBundle\Reporting\EventReportManager;
 
 class ReportController extends Controller
 {
     /**
      * @Route("/events/report/recentlyUpdated.csv")
      */
-    public function updatedEventsAction(){
+    public function updatedEventsAction()
+    {
 
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $reportingManager = new EventReportManager($em);
+        $reportingManager = $this->container->get('report_event_manager');
         $content = $reportingManager->getRecentlyUpdatedReport();
 
         $response = new Response($content);
-        $response->headers->set('Content-Type','text/csv');
+        $response->headers->set('Content-Type', 'text/csv');
         return $response;
     }
 
